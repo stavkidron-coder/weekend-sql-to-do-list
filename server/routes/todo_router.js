5e0055d38ea3bf85ad8fa45f4fb96aa353c8cdb6
Stav Kidron <kidronstav@gmail.com>
2020-10-16 17:37:27 -0500

    sanitized post

--- server/routes/todo_router.js
+++ server/routes/todo_router.js
@@ -2,13 +2,25 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+// router.get('/', (req, res) => {
+//     let queryText = `SELECT * FROM "todos";`;
+//     pool.query(queryText).then((result) => {
+//         res.send(result.rows);
+//     }).catch((error) => {
+//         console.log('ERROR in GET router', error);
+//         res.sendStatus(500);
+//     });
+// })
+
+
+
 router.post('/', (req, res) => {
     console.log('req.body:', req.body);
     
     let task = req.body.task;
 
-    let queryText = `INSERT INTO "todos" ("task") VALUES ('${task}');`;
-    pool.query(queryText).then((result) => {
+    let queryText = `INSERT INTO "todos" ("task") VALUES ($1);`;
+    pool.query(queryText, [task]).then((result) => {
         res.sendStatus(200);
     }).catch((error) => {
         console.log('ERROR in post router', error);
